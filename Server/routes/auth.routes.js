e183e8969a779bb908da4b25aaf33ee264577e95
Rabbi Hossain <rabbihossainlhp@gmail.com>
2026-03-20 16:38:57 +0600

    added logout

--- Server/routes/auth.routes.js
+++ Server/routes/auth.routes.js
@@ -1,11 +1,13 @@
 //dependencies.....
 const router = require('express').Router();
-const { loginController, registerController } = require('../controllers/authController');
+const { loginController, registerController, logoutController } = require('../controllers/authController');
+const authMiddleware = require('../middleware/auth.middleware');
 
 
 
 router.post('/register',registerController);
 router.post('/login', loginController);
+router.post('/logout',authMiddleware,logoutController);
 
 
 
