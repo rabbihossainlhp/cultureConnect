1ee245661f1ca8d8cdf033f98d89d6b055023380
Rabbi Hossain <rabbihossainlhp@gmail.com>
2026-05-06 10:18:36 +0600

    Inspection of redis config..

--- Server/config/redis.js
+++ Server/config/redis.js
@@ -4,14 +4,22 @@ const {createClient} = require('redis');
 
 const isProd = process.env.NODE_ENV === 'production';
 
-const redisUrl = isProd ? process.env.REDIS_URL : 'redis://localhost:6379';
+const redisUrl = isProd ? (process.env.REDIS_URL || "").trim() : 'redis://localhost:6379';
+
+
+if(!redisUrl){
+    throw new Error("Redus URL missing in production env.");
+}
+
 
 const client = createClient({
     url:redisUrl,
-    port:6379,
+
     socket:{
-        reconnectStrategy:(reties) =>{
-            const delay = Math.min(reties * 50,500);
+        connectTimeout:10000,
+        keepAlive:10000,
+        reconnectStrategy:(retries) =>{
+            const delay = Math.min(retries * 200,5000);
             return delay
         }
     }
@@ -21,23 +29,30 @@ const client = createClient({
 
 const connectRedisClient = async()=>{
     try{
-        await client.connect();
+        if(!client.isOpen){
+            await client.connect();
+        }
+        await client.ping();
         console.log("Redis Connected");
 
-        client.on('ready',()=>{
-            console.log('Redis Client Ready');
-        });
-
     }catch(err){
-        console.error('Redis connection error: ',err.message);
+        console.error('Redis connection error: ',err);
     }
 }
 
 
 
+client.on('ready',()=>{
+    console.log('Redis Client Ready');
+});
 
 client.on('error',(err)=>{
     console.error('Redis Client Error: ', err.message);
+});
+
+
+client.on('reconnecting',()=>{
+    console.log('Redis reconnecting....');
 });
 
 
